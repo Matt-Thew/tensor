cae0f2a4fc481c6ed569cf981f1e5cec5d9f4823
Matt <noppadolkhundam@Matts-MacBook-Pro.local>
2020-01-23 14:07:25 +0700

    data

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
-const tf = require("@tensorflow/tfjs-node");
-let { features, testFeatures, testFeatures } = loadCSV("cars.csv", {
-  dataColumns: ["passedemissions"]
+const tf = require("@tensorflow/tfjs");
+const loadCSV = require("./load-csv");
+let { features, testFeatures } = loadCSV("clvertab_200123.csv", {
+  dataColumns: ["profile_objectid", "profile_platform"]
 });
+
+console.log(features);
+const data = tf.tensor(features);
+console.log(data);
